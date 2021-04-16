82e026a4d8809bd8e994aa8199893a9b24bab872
SrAlch94 <daniel.contero@gmail.com>
2021-04-16 15:24:20 +0100

    TestPost

---
+++ Assessment01Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace CA_conteroDaniel
+{
+    class Assessment01Program
+    {
+        double miles, gallons, mpg, price;
+        string input, unitType;
+        string apikey = "3d6Mms9i06IQ2Hrcvb3lht:4D99ebw6IAeZEaLuBuKicL";
+        static void TESTMain(string[] args)
+        {
+            var calc = new Assessment01Program();
+
+            Console.WriteLine("Welcome to the Miles per Galon calculator!");
+            Console.WriteLine("Please, introduce the amount of Miles");
+            calc.miles = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, Introduce now the amount of Gallons");
+            calc.gallons = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Are this {calc.gallons} Uk or US Gallons?");
+            calc.unitType = Console.ReadLine();
+
+            Console.WriteLine("Is the vehicle Gasoline or Diesel?");
+            calc.input = Console.ReadLine();
+
+            calc.mpg = MPGCalculator(calc.miles, calc.gallons);
+            calc.price = PriceCalculator(calc.mpg, calc.gallons, calc.input);
+            Console.WriteLine($"{calc.price}");
+        }
+
+        static double MPGCalculator(double mileIN, double gallonIN)
+        {
+            return mileIN / gallonIN;   
+        }
+
+        static double PriceCalculator(double totalMPG, double totalGall, 
+                                      string carAnsw) 
+        {
+            double price = 6.62;
+            try
+            {
+                price = OnlinePriceCatch();
+                return price;
+            }
+            catch 
+            {
+                return price;
+            }
+        }
+
+        static double OnlinePriceCatch() 
+        {
+            double onlinePrice;
+            onlinePrice = 10 + 1;
+            return onlinePrice;
+        }
+    }
+
+}
---
+++ CodeFile1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+using unirest_net.http;
+
+namespace CA_conteroDaniel 
+{
+    class TestAPI
+    {
+        static void Main()
+        {
+            // The idea of implementing this as well as the min steps comes from
+            // https://rapidapi.com/blog/how-to-use-an-api-with-c-sharp/
+            //var client = Unirest.get("https://gas-price.p.rapidapi.com/europeanCountries");
+
+            HttpResponse<string> response = Unirest.get("https://gas-price.p.rapidapi.com/europeanCountries")
+                    .header("x-rapidapi-key", "b5db5ea616msh83542bd61fdd82cp10c4dejsn9d37827493ee")
+                    .header("x-rapidapi-host", "gas-price.p.rapidapi.com")
+                    //.header("Accept", "application/json")
+                    .asJson<string>();
+
+            
+            
+            
+            
+            XmlDocument xmlDoc = new XmlDocument(response.());
+
+            foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes) 
+            {
+                Console.WriteLine(node.InnerText);
+            }
+        }
+    }
+}
