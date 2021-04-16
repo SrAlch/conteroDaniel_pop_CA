b09d717c7f1a93ca12ca779c263fab6d26518708
SrAlch94 <daniel.contero@gmail.com>
2021-04-16 15:24:20 +0100

    TestPost

---
+++ Assessment01Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+namespace CA_conteroDaniel
+{
+    class Assessment01Program
+    {
+        double miles, gallons, mpg, price;
+        string input, unitType;
+        string apikey = "3d6Mms9i06IQ2Hrcvb3lht:4D99ebw6IAeZEaLuBuKicL";
+        static void Main(string[] args)
+        {
+            var calc = new Assessment01Program();
+
+            Console.WriteLine("Welcome to the Miles per Galon calculator!");
+            Console.WriteLine("Please, introduce the amount of Miles");
+            calc.miles = TryParseDouble(Console.ReadLine());
+
+            Console.WriteLine("Please, Introduce now the amount of Gallons");
+            calc.gallons = TryParseDouble(Console.ReadLine());
+
+            Console.WriteLine($"Are this {calc.gallons} Uk or US Gallons?");
+            calc.unitType = Console.ReadLine();
+
+            Console.WriteLine("Is the vehicle Gasoline or Diesel?");
+            calc.input = Console.ReadLine();
+
+            calc.mpg = MPGCalculator(calc.miles, calc.gallons);
+            calc.price = PriceCalculator(calc.mpg, calc.gallons, calc.input);
+            Console.WriteLine($"{calc.price}");
+        }
+
+        static double TryParseDouble(string input) 
+        {
+            double newVal;
+            try
+            {
+                if (double.Parse(input) > 0)
+                {
+                    newVal = double.Parse(input);
+                }
+                else 
+                {
+                    Console.WriteLine("The value need to be greater than 0");
+                    newVal = TryParseDouble(Console.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Please insert a valid value");
+                newVal = TryParseDouble(Console.ReadLine());
+            }
+            return newVal;
+        }
+        
+        static double MPGCalculator(double mileIN, double gallonIN)
+        {
+            return mileIN / gallonIN;   
+        }
+
+        static double PriceCalculator(double totalMPG, double totalGall, 
+                                      string carAnsw) 
+        {
+            double price = 6.62;
+            try
+            {
+                price = OnlinePriceCatch();
+                return price;
+            }
+            catch 
+            {
+                return price;
+            }
+        }
+
+        static double OnlinePriceCatch() 
+        {
+            double onlinePrice;
+            onlinePrice = 10 + 1;
+            return onlinePrice;
+        }
+    }
+
+}
---
+++ CodeFile1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Linq;
+using unirest_net.http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace CA_conteroDaniel 
+{
+    class TestAPI
+    {
+        public class Gas 
+        {
+            public string currency { get; set; }
+            public string lpg { get; set; }
+            public string diesel { get; set; }
+            public string gasoline { get; set; }
+            public string country { get; set; }
+
+        }
+
+        public class Result
+        {
+            //List<Gas> results = new List<Gas>();
+            public List<Gas> results { get; set; }
+        }
+
+        static void HttpRequest() 
+        {
+            var client = Unirest.get("https://gas-price.p.rapidapi.com/europeanCountries");
+
+            HttpResponse<string> response = Unirest.get("https://gas-price.p.rapidapi.com/europeanCountries")
+                    .header("x-rapidapi-key", "b5db5ea616msh83542bd61fdd82cp10c4dejsn9d37827493ee")
+                    .header("x-rapidapi-host", "gas-price.p.rapidapi.com")
+                    //.header("Accept", "application/json")
+                    .asJson<string>();
+
+            using JsonDocument doc = JsonDocument.Parse(response.Body);
+            JsonElement root = doc.RootElement;
+        }
+
+        static void JsonMain()
+        {
+            string path = @"D:\json\test.json";
+
+            string jsonFromFile;
+            using (var reader = new StreamReader(path)) 
+            {
+                jsonFromFile = reader.ReadToEnd();
+            }
+
+            Result gasListing = JsonConvert.DeserializeObject<Result>(jsonFromFile);
+            Console.WriteLine(gasListing.results[0].country);
+
+            List<Gas> test = gasListing.results;
+
+            foreach (Gas gas in test)
+            {
+                Console.WriteLine(gas.country);
+                Console.WriteLine(gas.diesel);
+            }
+
+
+        }
+    }
+}
